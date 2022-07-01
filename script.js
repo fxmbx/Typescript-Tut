3c36ddcae2e9d89e9ec93445bf340a5f59cee8fc
fxmbx <funbiolaore@gmail.com>
2022-07-01 23:45:12 +0100

    helps skip type checking during validation

---
+++ script.js
@@ -0,0 +1,10 @@
+var character = "paul";
+var age = 30;
+var isValid = false;
+// character = 30 error type number cant be assigned to string
+character = "sanwo";
+console.log(character);
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(10));
---
+++ script.ts
@@ -0,0 +1,13 @@
+let character = "paul";
+let age = 30;
+let isValid = false;
+
+// character = 30 error type number cant be assigned to string
+character = "sanwo";
+console.log(character);
+
+const circ = (diameter: number) => {
+  return diameter * Math.PI;
+};
+
+console.log(circ(10));
